70822bf0850baed1fd7a90f54b09d763012411ec
Jose D. Vasquez <jodavaga@Joses-iMac.local>
2020-08-12 21:18:32 -0500

    login form, custom validations. rut format on blur

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+
+import { rutFormat, rutValidate, rutClean } from 'rut-helpers';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px auto;
+  width: 300px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin-top: 10px;
+  width: 100%;
+`;
 
 function App() {
+
+  
+  const {register, handleSubmit, errors, getValues, setValue, formState} = useForm({
+    mode: 'onBlur'
+  });
+
+  const { dirtyFields } = formState;
+  
+  const onSubmit = (values, e) => {
+    e.preventDefault();
+    values.rut = rutClean(values.rut);
+    console.log(values);
+  }
+
+  const handleBlur = (e) => {
+    if(e.target.name === 'rut') {
+      const formatRut = rutFormat(getValues(e.target.name));
+      setValue(e.target.name, formatRut, { shouldValidate: true });
+    }
+  }
+
+  // console.log(errors);
+  console.log(dirtyFields);
+
+  const validations = {
+    required: {
+      value: true,
+      message: 'Campo requerido'
+    },
+    minLength: {
+      value: 3,
+      message: 'Minimo de chars'
+    }, 
+    validate: value => rutValidate(value) || 'Rut validation failed'
+  }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input 
+          type="text"
+          name="rut"
+          placeholder="Rut"
+          ref={register(validations)}
+          onBlur={handleBlur}
+        />
+
+        {errors?.rut && <small>{ errors.rut.message }</small>}
+        <Input 
+          type="text"
+          name="pass"
+          placeholder="password"
+          ref={register}
+        />
+        {errors?.pass && <small>{ errors.pass.message }</small>}
+
+        <button style={{marginTop: 10, padding: 10, width: 100+'%', boxSizing: 'border-box'}}>Submit</button>
+      </Form>
     </div>
   );
 }
